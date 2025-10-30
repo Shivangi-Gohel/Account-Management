2ccb1667d9063ca46b9084286be8d74d94cec60b
Shivangi-Gohel <shivangigohel18@gmail.com>
2025-10-30 13:37:28 +0530

    add jwt services

---
+++ backend/index.js
@@ -0,0 +1,18 @@
+import express from 'express';
+import { connectDB } from './config/db.js';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app = express();
+connectDB();
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('API is running...');
+});
+
+const PORT = process.env.PORT || 8000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
