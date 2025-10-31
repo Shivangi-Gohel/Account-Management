c3f653023804dab0e1ef5a1f445325e33e2f69d5
Shivangi-Gohel <shivangigohel18@gmail.com>
2025-10-31 12:46:50 +0530

    upfdate import

--- frontend/src/components/ProtectedRoute.jsx
+++ frontend/src/components/ProtectedRoute.jsx
@@ -1,5 +1,5 @@
 import { Navigate, Outlet } from "react-router-dom";
-import { useAuth } from "../context/AuthContext.jsx";
+import { useAuth } from "../context/authContext.jsx";
 
 const ProtectedRoute = () => {
   const { user, loading } = useAuth();
