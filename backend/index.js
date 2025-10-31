1ab69a2da29aba1128e300709a3522459e0b556f
Shivangi-Gohel <shivangigohel18@gmail.com>
2025-10-31 13:20:25 +0530

    update CORS configuration to allow requests

--- backend/index.js
+++ backend/index.js
@@ -9,8 +9,8 @@ const app = express();
 connectDB();
 
 app.use(cors({
-  origin: "http://localhost:5173",  // your frontend URL
-  credentials: true,                // allow cookies / credentials
+  origin: ["http://localhost:5173", "https://account-management-two.vercel.app"],  
+  credentials: true,        
 }))
 
 app.use(express.json({ limit: "10mb" }));
