954775ab1e5f0c6df044ae88c3ecd8e0fc566792
Shivangi-Gohel <shivangigohel18@gmail.com>
2025-10-31 13:14:52 +0530

    update API URL to production endpoint

--- frontend/src/constant.js
+++ frontend/src/constant.js
@@ -1,1 +1,2 @@
-export const URL = "http://localhost:8000/api/auth";
+// export const URL = "http://localhost:8000/api/auth";
+export const URL = "https://account-management-thk7.onrender.com/api/auth";
