2e546c22f70499036b86563f3046a29995af9009
Shivangi-Gohel <shivangigohel18@gmail.com>
2025-10-31 13:28:47 +0530

    update CORS configuration to restrict allowed origins

--- backend/index.js
+++ backend/index.js
@@ -9,7 +9,7 @@ const app = express();
 connectDB();
 
 app.use(cors({
-  origin: ["http://localhost:5173", "https://account-management-two.vercel.app"],  
+  origin: "https://account-management-two.vercel.app",  
   credentials: true,        
 }))
 
